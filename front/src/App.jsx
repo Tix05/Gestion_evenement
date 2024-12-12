fc7543dbd5da6cffb1aa8490fb89ea80c3bf42ec
Tix05 <150007929+Tix05@users.noreply.github.com>
2024-12-12 15:35:04 +0300

    front kely

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './pages/page_participant/login';
 import Acceuil from './pages/page_participant/Acceuil';
 import Apropos from './pages/page_participant/Apropos';
 import Dashboard from '../../front/src/pages/page_organisateur/Dashboard';
+import ListeEvenement from './pages/page_organisateur/ListeEvenement';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Route path='/' element={<Acceuil />} />
       <Route path='/a-propos' element={<Apropos />} />
       <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/list-event' element={<ListeEvenement />} />
     </Routes>
   )
 }
---
+++ front/src/components/Menunavbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Avatar } from 'primereact/avatar';
+import { FiBell } from 'react-icons/fi'; // Importation de l'icône de cloche
+import { AiOutlineSearch } from 'react-icons/ai'; // Nouvelle icône de recherche
+
+const Menunavbar = () => {
+    return (
+        <div className="flex w-full items-center justify-between h-14 bg-purple-200 px-4">
+            <div className="flex items-center justify-center p-5 w-full max-w-md">
+                <div className="rounded-lg p-2 w-full">
+                    <div className="flex">
+                        <div className="flex w-10 items-center justify-center rounded-tl-lg rounded-bl-lg bg-white p-2">
+                            <AiOutlineSearch className="w-5 h-5 text-gray-500" />
+                        </div>
+                        <input
+                            type="text"
+                            className="w-full max-w-[160px] bg-white pl-2 font-semibold outline-none border-none"
+                            placeholder="Rechercher..."
+                        />
+                        <input
+                            type="button"
+                            value="Chercher"
+                            className="bg-blue-500 p-2 rounded-tr-lg rounded-br-lg text-white font-bold hover:bg-blue-800 transition-colors border-none outline-none"
+                        />
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="flex items-center space-x-4">
+                <button className="p-[2px] rounded-full hover:bg-purple-300">
+                    <FiBell className="text-xl text-gray-800" />
+                </button>
+                <Avatar
+                    image="https://randomuser.me/api/portraits/men/41.jpg"
+                    size="large"
+                    shape="circle"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Menunavbar;
